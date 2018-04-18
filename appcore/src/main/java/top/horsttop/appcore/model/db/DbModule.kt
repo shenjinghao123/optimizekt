c3e7cda485b90bfe28c56b4a7825ae437e84ea2d
horsttop <ff7df2>
2018-04-18 12:09:00 +0800

    `Moshi For Kotlin` `Moshi + Retrofit`

---
+++ appcore/src/main/java/top/horsttop/appcore/model/db/DbModule.kt
@@ -0,0 +1,45 @@
+package top.horsttop.appcore.model.db
+
+import android.app.Application
+import android.arch.persistence.db.SupportSQLiteOpenHelper
+import android.arch.persistence.db.framework.FrameworkSQLiteOpenHelperFactory
+
+import com.squareup.sqlbrite3.BriteDatabase
+import com.squareup.sqlbrite3.SqlBrite
+
+import javax.inject.Singleton
+
+import dagger.Module
+import dagger.Provides
+import io.reactivex.schedulers.Schedulers
+import timber.log.Timber
+import top.horsttop.appcore.BuildConfig
+
+/**
+ * Created by horsttop on 2018/4/16.
+ */
+
+@Module
+class DbModule {
+    @Provides
+    @Singleton
+    internal fun provideSqlBrite(): SqlBrite {
+        return SqlBrite.Builder()
+                .logger { message -> Timber.tag("Database").v(message) }
+                .build()
+    }
+
+    @Provides
+    @Singleton
+    internal fun provideDatabase(sqlBrite: SqlBrite, application: Application): BriteDatabase {
+        val configuration = SupportSQLiteOpenHelper.Configuration.builder(application)
+                .name(BuildConfig.DB_NAME)
+                .callback(DbCallback())
+                .build()
+        val factory = FrameworkSQLiteOpenHelperFactory()
+        val helper = factory.create(configuration)
+        val db = sqlBrite.wrapDatabaseHelper(helper, Schedulers.io())
+        db.setLoggingEnabled(true)
+        return db
+    }
+}
--- appcore/src/main/java/top/horsttop/appcore/module/NetworkModule.kt
+++ appcore/src/main/java/top/horsttop/appcore/module/NetworkModule.kt
@@ -1,48 +1,57 @@
 package top.horsttop.appcore.module
 
-import com.google.gson.Gson
-import com.google.gson.GsonBuilder
+import com.squareup.moshi.KotlinJsonAdapterFactory
+import com.squareup.moshi.Moshi
 import dagger.Module
 import dagger.Provides
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
 import top.horsttop.appcore.BuildConfig
+import top.horsttop.appcore.network.RetrofitInterceptor
 import javax.inject.Singleton
 
 /**
  * Created by horsttop on 2018/4/13.
  */
-//@Module
-//class NetworkModule{
-//
-//
-//    @Provides
-//    @Singleton
-//    internal fun provideGson(): Gson {
-//        return GsonBuilder().serializeNulls().registerTypeAdapterFactory(AutoValueGsonFactory.create()).create()
-//    }
-//
-//    @Provides
-//    @Singleton
-//    internal fun provideOkHttpClient(): OkHttpClient {
-//        return OkHttpClient.Builder().addInterceptor(RetrofitInterceptor()).build()
-//    }
-//
-//    @Provides
-//    @Singleton
-//    internal fun provideGsonConverterFactory(gson: Gson): GsonConverterFactory {
-//        return GsonConverterFactory.create(gson)
-//    }
-//
-//    @Provides
-//    @Singleton
-//    internal fun provideRetrofit(okHttpClient: OkHttpClient, factory: GsonConverterFactory): Retrofit {
-//        return Retrofit.Builder()
-//                .baseUrl(BuildConfig.END_POINT)
-//                .client(okHttpClient)
-//                .addConverterFactory(factory)
-//                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-//                .build()
-//    }
-//}
+@Module
+class NetworkModule {
+
+    @Provides
+    @Singleton
+    internal fun ofMoshi(): Moshi {
+        return Moshi.Builder()
+                // Add any other JsonAdapter factories.
+                .add(KotlinJsonAdapterFactory())
+                .build()
+    }
+
+    @Provides
+    @Singleton
+    internal fun ofOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+                .addInterceptor(RetrofitInterceptor())
+                .addInterceptor(HttpLoggingInterceptor()
+                        .setLevel(HttpLoggingInterceptor.Level.BODY))
+                .build()
+    }
+
+    @Provides
+    @Singleton
+    internal fun ofMoshiConverterFactory(moshi: Moshi): MoshiConverterFactory {
+        return MoshiConverterFactory.create(moshi)
+    }
+
+    @Provides
+    @Singleton
+    internal fun ofRetrofit(okHttpClient: OkHttpClient, factory: MoshiConverterFactory): Retrofit {
+        return Retrofit.Builder()
+                .baseUrl(BuildConfig.END_POINT)
+                .client(okHttpClient)
+                .addConverterFactory(factory)
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .build()
+    }
+}
